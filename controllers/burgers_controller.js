b92b2f11d196a9cf0e9c60a5fc3534090b22d925
Veronica <chingona27>
2018-05-15 17:00:40 -0500

    controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+router.get('/', function(req,res) {
+    res.redirect('/burgers')
+});
+
+router.get('/burgers', function(req, res){
+    burger.all(function (data){ 
+        var hbsObject = {}
+    })
+});
