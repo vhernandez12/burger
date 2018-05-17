50113ae3d255be8b92bd1fa9b6191761f322e550
Veronica <chingona27>
2018-05-17 16:55:29 -0500

    controllers update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,15 +3,32 @@ var bodyParser = require('body-parser');
 var router = express.Router();
 var burger = require('../models/burger.js');
 
-router.get('/', function(req,res) {
+router.get('/', function (req, res) {
     res.redirect('/burgers')
 });
 
-router.get('/burgers', function(req, res){
-    burger.all(function (data){ 
-        var hbsObject = { burgers:data}
-        res.render ('index', hbsObject);
+router.get('/burgers', function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = { burgers: data }
+        res.render('index', hbsObject);
     });
 });
 
-router.put('/')
+router.post('/burgers/create', function (req, res) {
+    console.log(req.body.burger_name, "=entered name for burger")
+    burger.create('burger_name', req.body.burger_name, function () {
+        res.redirect('/burgers');
+    });
+});
+
+router.put('/burgers/update/:id', function(req,res) {
+    var condition = 'id = ' + req.params.id;
+    
+    console.log('condition', condition);
+
+    burger.update({ devaoured: req.body.devoured}, condition, function() {
+        res.redirect('/burgers');
+    });
+});
+
+module.exports = router;
